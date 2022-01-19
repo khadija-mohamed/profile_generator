bb2812944f88dfec3e81c4bcbdc20f6f176d1a36
Khadija Mohamed <khadijam@live.ca>
2022-01-19 04:32:00 -0500

    Survey for app

---
+++ survey.js
@@ -0,0 +1,48 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What do you think of Node.js? ', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+}, 
+r2.question("What's your name? Nicknames are also acceptable", (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+  
+  r2.close();
+}, 
+
+r3.question('What do you listen to while doing that ', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+  
+  r3.close();
+},
+
+r4.question('Which meal is your favourte (eg: dinner, brunch, etc)', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+  
+  r4.close();
+
+},
+r5.question("What's your favourite thing to eat for that meal?", (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+  
+  r5.close();
+},
+
+r6.question('Which sport is your absolute favourite?', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+  
+  r6.close();
+},
+
+r7.question('What is your superpwer? In a few words, tell us what you are amazing at', (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+  
+  r7.close();
+
+})))))));; 
